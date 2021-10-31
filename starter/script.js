ff9600a24f2ccd4ce2d647b8ff64aa105daae0d3
okiki123 <okikimalomo17@gmail.com>
2021-11-01 00:48:24 +0100

    Initial Commit

---
+++ starter/script.js
@@ -0,0 +1,73 @@
+// let js = 'amazing';
+// // console.log(40 + 8 + 23 - 10);
+
+// // console.log('jonas');
+// // console.log(23);
+
+// // let firstName = 'Jonas'
+// // console.log(firstName);
+
+
+// // let javascriptIsFun = true;
+// // console.log(javascriptIsFun);
+
+// // let
+
+// const ageJonas = 2037 - 1991;
+// const ageSarah = 2037 - 2018;
+// console.log(ageJonas, ageSarah);
+
+// const firstName = 'Okiki';
+// const lastName = 'Malomo';
+// console.log(firstName + " " + lastName);
+
+// console.log(ageSarah >= 18);
+// console.log(ageSarah > ageJonas);
+
+// const averageAge = (ageJonas + ageSarah) / 2 
+// // console.log(averageAge);
+
+// let markHeight = 1.69;
+// let markMass = 78;
+// let johnHeight = 1.95;
+// let johnMass = 92;
+
+// markBMI = markMass/(markHeight ** 2);
+// johnBMI = johnMass/(johnHeight ** 2);
+
+// if(markBMI > johnBMI) {
+//     console.log(`Mark's BMI(${markBMI}) is higher than John's BMI(${johnBMI})`);
+// } else {console.log(`Mark's BMI(${markBMI}) is lesser than John's BMI(${johnBMI})`)};
+
+// // const firstName = 'Okiki';
+// // const job = 'student';
+// // const birthYear = 1991;
+// // const year = 2037;
+
+// // const okiki = 'I am ' + firstName + ', a' + (year - birthYear) + job
+
+// // const okikiNew = `I am ${firstName}, a ${year - birthYear} year old ${job}!`;
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough) {
+//     console.log('Sarah can start driving license');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years`)
+// }
+
+// const scoreDolphins = (96 + 108 + 89)/3;
+// const scoreKoalas = (88 + 91 + 118)/3;
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins wins the trophy');
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log('Kolas wins the trophy');
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy')  
+// }
+  
+
+ 
